2e6474fea1ebda9ad7af68685563fcfa5c96022b
santos22 <santosjavier22@gmail.com>
2015-10-19 14:37:40 -0500

    Can iterate throug JSON Array and print out the elements...learning JSON simple on the fly. Next up: parsing sections info I need to succeed

--- Clever.java
+++ Clever.java
@@ -3,10 +3,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
 
-// import org.json.simple.JSONObject;
-// import org.json.simple.JSONArray;
-// import org.json.simple.parser.ParseException;
-// import org.json.simple.parser.JSONParser;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+import java.util.Iterator;
 
 import java.io.*;
 
@@ -14,16 +15,29 @@ public class Clever {
 
 	public static void main(String[] args) {
 		try {
-		    String body = Request.Get("https://api.clever.com/v1.1/districts/4fd43cc56d11340000000005/students")
+		    String body = Request.Get("https://api.clever.com/v1.1/sections")
                      .addHeader("Authorization", "Bearer DEMO_TOKEN")
                      .execute().returnContent().asString();
 		
-			//System.out.println(body);
-			//String jsonEX = {"balance": 1000.21, "num":100, "is_vip":true, "name":"foo"}
+			JSONParser parser = new JSONParser();
+
+			JSONObject jsonObject = (JSONObject) parser.parse(body);
+
+			JSONArray response = (JSONArray) jsonObject.get("data");
+
+			for(int i = 0; i < response.size(); i ++)
+			{
+				System.out.println("The " + i + " element of the array: "+response.get(i));
+			}
+
+			// System.out.println(body);
 
 		} catch (IOException e) {
 		    e.printStackTrace();
 		    // Or ask the user for a different filename...
+		} catch(ParseException pe){
+		    System.out.println("position: " + pe.getPosition());
+		    System.out.println(pe);
 		}
 	}
 }
@@ -31,3 +45,4 @@ public class Clever {
 // commands to run
 // javac -cp \* Clever.java
 // java -cp .:\* Clever
+// http://examples.javacodegeeks.com/core-java/json/java-json-parser-example/
