c7feb74ee730a509240246161da746b9f92acd77
santos22 <santosjavier22@gmail.com>
2015-10-20 17:47:28 -0500

    Calculates the average number of students per section :)

--- Clever.java
+++ Clever.java
@@ -7,9 +7,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
 import org.json.simple.parser.JSONParser;
-import java.util.Iterator;
-
-import java.io.*;
 
 public class Clever {
 
@@ -23,30 +20,27 @@ public class Clever {
 
 			JSONObject jsonObject = (JSONObject) parser.parse(body);
 
-			JSONObject structure = (JSONObject) jsonObject.get("paging");
-			System.out.println("Total " + structure.get("total"));
-			System.out.println("Count " + structure.get("count"));
+			JSONObject sections = (JSONObject) jsonObject.get("paging");
+			String totalSections = sections.get("total").toString();
+			String totalStudents = sections.get("count").toString();
 
-			JSONArray response = (JSONArray) jsonObject.get("links");
+			// parse string values
+			int numberOfSections = Integer.parseInt(totalSections);
+			double numberOfStudents = Double.parseDouble(totalStudents);
 
-			for(int i = 0; i < response.size(); i ++)
-			{
-				System.out.println("The " + i + " element of the array: "+response.get(i));
-			}
+			double average = numberOfStudents / numberOfSections;
 
-			// System.out.println(body);
+			System.out.println("Total sections: " + numberOfSections);
+			System.out.println("Total students: " + numberOfStudents);
+
+			// average number of students per section
+			System.out.println("Average number of students per section: " + average);
 
 		} catch (IOException e) {
 		    e.printStackTrace();
-		    // Or ask the user for a different filename...
 		} catch(ParseException pe){
 		    System.out.println("position: " + pe.getPosition());
 		    System.out.println(pe);
 		}
 	}
 }
-
-// commands to run
-// javac -cp \* Clever.java
-// java -cp .:\* Clever
-// http://examples.javacodegeeks.com/core-java/json/java-json-parser-example/ *** GOD BLESS THIS PERSON ***
